c180ac497c863cee059bd38ab73f58c0033fdd75
ori190 <oriana.aguado@davinci.edu.ar>
2024-09-04 20:58:52 -0300

    fix responsive styles

--- src/organisms/Presentation/Presentation.styles.ts
+++ src/organisms/Presentation/Presentation.styles.ts
@@ -1,14 +1,14 @@
 export const presentationStyles = {
     width: '100%',
-    height: { base: '100vh', md: '90vh%' },
+    height: { base: 'auto', md: '90vh' },
     backgroundColor: 'primary.500',
     backgroundSize: {
-        base: '70%, cover',
-        md: '50%, cover',
+        base: '65%, cover',
+        md: '40%, cover',
         xl: 'auto, cover',
     },
     backgroundPosition: {
-        base: 'center, center',
+        base: '50% 40%, center',
         md: '90% 40%, center',
         xl: '80% 40%, center',
     },
@@ -20,7 +20,7 @@ export const rightBoxStyles = {
     height: '100%',
     display: 'flex',
     paddingLeft: { base: '0', md: '100px', xl: '200px' },
-    paddingBottom: { base: '0', md: '10rem' },
+    paddingBottom: { base: '0', md: '7rem', xl: '10rem' },
     position: 'relative',
     flexWrap: 'wrap',
     justifyContent: 'center',
@@ -30,22 +30,23 @@ export const rightBoxStyles = {
 export const titleBoxText = {
     position: 'absolute',
     left: { base: '10%', md: '46px', xl: '200px' },
-    top: { base: '5%', md: '50px' },
+    top: { base: '40px', md: '50px' },
     height: 'fit-content',
 };
 
 export const subtitleBoxText = {
     position: 'absolute',
-    left: { base: 'auto', md: '300px', xl: '430px' },
-    top: { base: '18%', md: '260px' },
+    left: { base: '40%', md: '300px', xl: '430px' },
+    top: { base: '150px', md: '200px' },
     height: 'fit-content',
     width: { base: '200px', md: 'auto' },
 };
 
 export const descriptionBoxText = {
-    position: 'absolute',
-    left: { base: 'auto', md: '100px', xl: '200px' },
-    top: '500px',
+    position: { base: 'auto', md: 'absolute' },
+    left: { base: 'auto', md: '50px', xl: '200px' },
+    top: { base: 'auto', md: '380px', xl: '435px' },
     height: 'fit-content',
+    marginTop: { base: '140%', md: '0' },
     maxWidth: { base: '80%', md: '500px' },
 };
